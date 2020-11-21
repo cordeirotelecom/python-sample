a0ac869af2db95a5d2dadd7eefc9d9a9b94268c3
Vagner Cordeiro <43836503+cordeirotelecom@users.noreply.github.com>
2020-11-21 08:30:33 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,52 @@
-imposto = 0.27
-salario = 3500.00
-print(f"imposto: {salario * imposto}")
+# Projeto de uma calculadora em Phyton
+
+def calculadora():
+    operador = input('''
+Exemplo: 1 + 1
+Resultado: 2
+
+OPERADORES:
++ para adição
+- subtração
+* multiplicação'
+/ divisão
+''')
+    
+    numero_1 = float(input('Entre com o primeiro número: '))
+    numero_2 = float(input('Entre com o segundo número: '))
+
+    if operacao == '+':
+        print('{} + {} = '.format(numero_1, numero_2))
+        print(numero_1 + numero_2)
+
+    elif operacao == '-':
+        print('{} - {} = '.format(numero_1, numero_2))
+        print(numero_1 - number_2)
+
+    elif operacao == '*':
+        print('{} * {} = '.format(numero_1, numero_2))
+        print(numero_1 * number_2)
+
+    elif operacao == '/':
+        print('{} / {} = '.format(numero_1, numero_2))
+        print(numero_1 / number_2)
+
+    else:
+        print('Você não entrou com uma operação válida, por favor rode o programa novamente.')
+
+    novamente()
+
+def novamente():
+    calc_novamente = input('''
+Você deseja calcular novamente?
+"S" para sim, "N" para não.
+''')
+
+    if calc_novamente.upper() == 'S':
+        calculadora()
+    elif calc_novamente.upper() == 'N':
+        print('Vá com Deus! Deus te Ilumine!')
+    else:
+        novamente()
+
+calculadora()
